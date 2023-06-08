4dc2293626f42785b9d59cf42d912cf600b426f3
tom (tux) <thd@mnet-online.de>
2023-06-08 09:14:02 +0200

    Removed some dead code, tried to improve readability

--- src/http_handler.cpp
+++ src/http_handler.cpp
@@ -15,15 +15,19 @@ hash(const char* str){
   return hash;
 }
 #endif
+/**
+ *  Handle general requests for the web-server. Basically:
+ *  - split path from extension
+ *  - hash the path and append the extension
+ *  - serve the file HASH.EXT with the mime-type of EXT.
+ *  - or fail, if it does not exist.
+ */
 void onRequest(AsyncWebServerRequest *request) {
   const char *url = request->url().c_str();
   msLastEvent = millis();
 #ifdef WITH_SPIFFS
   if(!strcmp(url, "/"))
     url="/portal.html";
-  //Serial.printf("request for %s\n", url);
-
-  //unsigned long long hs = hash( url );
   // We need to grab the extension before hashing, because otherwise
   // the MIME-type cannot be set.
   char *ext = strrchr(url,'.');
@@ -34,7 +38,6 @@ void onRequest(AsyncWebServerRequest *request) {
   }
   char buf[256];
   // Construct name of file in SPIFFS.
-  //sprintf(buf, "Request for %s, hash is %016llx%s\n", url, hs, ext);
   snprintf(buf, sizeof(buf), "/%016llx%s", hash(url), ext);
   if(SPIFFS.exists(buf)){
     AsyncWebServerResponse *response;
@@ -55,7 +58,7 @@ void onRequest(AsyncWebServerRequest *request) {
   }
   else
     request->send(404,"text/html", "Not found: " + request->url());
-#else
+#else  // Will we ever go back to no SPIFFS? this can be pruned?
   if(!strcmp(url, "/demo01.html")){
     DEBUG_MSG("Request for /demo02");
     sendZipped(request, demo01_html_gz, demo01_html_gz_len);
@@ -90,10 +93,11 @@ void onRequest(AsyncWebServerRequest *request) {
     request->send(404,"text/html", "Not found: " + request->url());
   }
 #endif
- //request->redirect("/");
 }
 
-
+/**
+ * 
+ */
 void onConfig(AsyncWebServerRequest *request){
   msLastEvent = millis();
   DEBUG_MSG("onConfig request\n");
@@ -109,13 +113,18 @@ void onConfig(AsyncWebServerRequest *request){
     savePreferences();
   }
 }
+/**
+ *
+ */
+void onScan(AsyncWebServerRequest *request) {
 #define TEMPL "<tr><td><input type='checkbox' name='blazers' checked='true'/>"\
   "</td><td><input value='%s'/></td><td><input value='%s'/></td></tr>"
-void onScan(AsyncWebServerRequest *request) {
+  char buf[256];
+
   msLastEvent = millis();
   DEBUG_MSG("onScan Request\n");
+  // Find all peers that advertise debug on udp
   int nrOfServices = MDNS.queryService("debug", "udp");
-  char buf[256];
 
   String rsp  = "<!DOCTYPE HTML><html lang='de'><head><title>Config Page</title>"
     "<meta name='viewport' content='width=device-width, initial-scale=1'>"
@@ -125,99 +134,17 @@ void onScan(AsyncWebServerRequest *request) {
     "</head>"
     "<body><h1>ESP WLAN Peers</h1><center>"
     "<table id='BlazerTable'>";
+  // Put myself in the list
   snprintf(buf, 256, TEMPL, WiFi.localIP().toString().c_str(), myName);
   rsp += buf;
-
+  // Add everyone found in local net
   for (int i = 0; i < nrOfServices; i=i+1) {
     snprintf(buf, 256, TEMPL, MDNS.IP(i).toString().c_str(), MDNS.hostname(i));
     rsp += buf;
   }
+  // And send page
   rsp +=
     "</table><input type='submit' value='Submit by IP' onclick='launch(\"portal.html\")'>"
-    "<br><input type='submit' value='Submit by name' onclick='launch2(\"portal.html\")'></center></body></html>";
+    "<br><input type='submit' value='Submit by name' onclick='launch2(\"portal.html\")'></center></body></html>"; 
   request->send(200, "text/html", rsp);
-
-#if 0
-  if( ON_AP_FILTER(request) ){ msAPActivity = millis(); }
-  if ( (request->hasParam("NW", true)) && (request->hasParam("HNAME", true))) {
-    DEBUG_MSG("Looks good\n");
-    String NW, pw, hname;
-    NW = "" + request->getParam("NW", true)->value();
-    if( request->getParam("PASSWD", true) ){
-      pw = "" + request->getParam("PASSWD", true)->value();
-    }
-    else{
-      pw = "";
-    }
-    hname = "" + request->getParam("HNAME", true)->value();
-    strncpy(extSSID,   request->getParam("NW", true)->value().c_str(), sizeof(extSSID));
-    strncpy(extPasswd, request->getParam("PASSWD", true)->value().c_str(), sizeof(extPasswd));
-    strncpy(myName,    request->getParam("HNAME", true)->value().c_str(), sizeof(myName));
-    DEBUG_MSG("TryWiFi on %s/%s and hName %s\n", extSSID, extPasswd, myName);
-    request->send(200, "text/html", "OK, try to connect" );
-    tryExt = true;
-  }
-  else if ( (request->hasParam("HNAME", true)) ) {
-    String  hname = "" + request->getParam("HNAME", true)->value();
-    strncpy(myName, hname.c_str(), sizeof(myName));
-    DEBUG_MSG("Changing name to %s\n", myName);
-    request->send(200, "text/html", "OK, try to connect" );
-    tryExt = true;
-  }
-  else {
-    Serial.println("Request for host " + request->host() + ", path " + request->url() );
-   String rsp  = "<!DOCTYPE HTML><html lang='de'><head><title>Config Page</title>"
-        "<meta name='viewport' content='width=device-width, initial-scale=1'>"
-        "<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>"
-        "<script src='blazer2.js'></script>"
-        "</head>"
-        "<body><h1>ESP WLAN Config</h1><form action='/scan' method='post'>"
-        "<table>"
-        "<tr><td>My Name</td><td><input type='text' name='HNAME' value='" + String(myName) + "' /></td></tr>";
-    int n = WiFi.scanComplete();
-    if (n == -2) {
-      WiFi.scanNetworks(true);
-    } else if (n) {
-      for (int i = 0; i < n; ++i) {
-        rsp += "<tr><td>" + String(i) + "</td>"
-               "<td>" + WiFi.SSID(i) + "</td>"
-               "<td><input type='radio' name='NW' value='" + WiFi.SSID(i) + "'/></td>"
-               "<td>" + String(WiFi.RSSI(i)) + "</td></tr>";
-      }
-      rsp += "<tr><td>Password</td><td><input type='password' name='PASSWD'/></td></tr>";
-      WiFi.scanDelete();
-      if (WiFi.scanComplete() == -2) {
-        WiFi.scanNetworks(true);
-      }
-    }
-    rsp +=
-      "</table><input type='submit' value='Submit'></form></body></html>";
-    request->send(200, "text/html", rsp);
-    rsp = String();
-  }
-#endif
 }
-
-#if 0
-/**
- * Set credentials to home-net
- */
-void onDevHome(AsyncWebServerRequest *request) {
-  DEBUG_MSG("Request for /devhome");
-  strcpy(extSSID, HOMESSID);
-  strcpy(extPasswd, HOMEPASSWD);
-  request->send(200, "text/html", "OK, try to connect" );
-  // Here we save preferences unconditionally
-  savePreferences();
-  tryExt = true;
-}
-void onDevHall(AsyncWebServerRequest *request) {
-  DEBUG_MSG("Request for /devhall");
-  strcpy(extSSID, HALLESSID);
-  strcpy(extPasswd, HALLEPASSWD);
-  request->send(200, "text/html", "OK, try to connect" );
-  // Here we save preferences unconditionally
-  savePreferences();
-  tryExt = true;
-}
-#endif
